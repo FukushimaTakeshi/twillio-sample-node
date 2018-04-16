3f42a816a35f9e4f543ceda82ce5617e13ac9d8c
FukushimaTakeshi <whippet.818@gmail.com>
2018-04-16 22:27:37 +0900

    changed to env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These vars are your accountSid and authToken from twilio.com/user/account
-const accountSid = 'ACd47fec33b5186a1a24c522e70e967d21';
-const authToken = 'e6256ee1619e013f77e67fd92efbd83b';
+var accountSid = process.env.TWILIO_ACCOUNT_SID;
+var authToken = process.env.TWILIO_AUTH_TOKEN;
 
 //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
@@ -10,8 +10,8 @@ exports.handler = function(event, context) {
   client.calls.create(
     {
       url: 'http://demo.twilio.com/docs/voice.xml',
-      to: '+818061400882',
-      from: '+815031963578',
+      to: process.env.TWILIO_MY_PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
     },
     (err, call) => {
       console.log(call.sid);
